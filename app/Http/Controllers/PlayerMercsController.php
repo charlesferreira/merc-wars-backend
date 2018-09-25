abaa95c791ba06676250e1c2d0996a1f2ad85734
Charles Ferreira <charles6286@gmail.com>
2018-09-25 20:58:53 -0300

    Comenta um método que não está sendo usado agora, mas está com erro de qualquer forma

--- app/Http/Controllers/PlayerMercsController.php
+++ app/Http/Controllers/PlayerMercsController.php
@@ -15,13 +15,13 @@ class PlayerMercsController extends Controller
         return response()->json($data);
     }
 
-    public function store($playerId)
-    {
-        $player = Player::find($playerId);
+    // public function store($playerId)
+    // {
+    //     $player = Player::find($playerId);
 
-        $merc = $player->createMerc(request('name'), request('skin'), request('weapon'));
-        $data = fractal($merc, MercTransformer::class)->ToArray()['data'];
+    //     $merc = $player->createMerc(request('name'), request('skin'), request('weapon'));
+    //     $data = fractal($merc, MercTransformer::class)->ToArray()['data'];
 
-        return response()->json($data, 201);
-    }
+    //     return response()->json($data, 201);
+    // }
 }

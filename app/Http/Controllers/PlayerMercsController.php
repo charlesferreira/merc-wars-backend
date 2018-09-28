3088359b2963b1e5db066105c033c673233c26b8
Charles Ferreira <charles6286@gmail.com>
2018-09-27 21:33:26 -0300

    Melhora as rotas

--- app/Http/Controllers/PlayerMercsController.php
+++ app/Http/Controllers/PlayerMercsController.php
@@ -17,35 +17,6 @@ class PlayerMercsController extends Controller
         return response()->json($data);
     }
 
-    public function show($playerId, $mercId)
-    {
-        if (!$player = Player::find($playerId)) {
-            return response()->json(['error' => 'Player Not Found'], 404);
-        }
-
-        if (!$merc = $player->mercs()->find($mercId)) {
-            return response()->json(['error' => 'Merc Not Found'], 404);
-        }
-
-        $data = fractal($merc, MercTransformer::class)->toArray()['data'];
-        return response()->json($data);
-    }
-
-    public function sendForHiring($playerId, $mercId)
-    {
-        if (!$player = Player::find($playerId)) {
-            return response()->json(['error' => 'Player Not Found'], 404);
-        }
-
-        if (!$merc = $player->mercs()->find($mercId)) {
-            return response()->json(['error' => 'Merc Not Found'], 404);
-        }
-
-        $merc->enableForHiring();
-        $data = fractal($merc, MercTransformer::class)->toArray()['data'];
-        return response()->json($data);
-    }
-
     // public function store($playerId)
     // {
     //     $player = Player::find($playerId);
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'players',
         'players/*',
+        'mercs/*',
         'github/*',
     ];
 }
